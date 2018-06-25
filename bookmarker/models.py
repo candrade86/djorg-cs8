ce30d2ef3e8712eae46d7845c4883fbbc255a9e7
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-25 18:44:18 -0400

    completed

--- bookmarker/models.py
+++ bookmarker/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from uuid import uuid4
 
-class Note(models.Model):
+class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    # TODO: add user//authoer who created it
+    
     title = models.CharField(max_length=200)
-    content = models.TextField(blank=True)
+    url_marker = models.URLField(max_length=200)
+    text_clip = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    # TODO: Tagging system or categories
+    
