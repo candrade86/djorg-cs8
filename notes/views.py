9b0bc0258d2a5385214f2308d0a748c567afd93c
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-26 15:50:46 -0400

    can print notes to screen

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Note
+
+
+def index(request):
+    notes_list = Note.objects.all()
+    print(notes_list)
+    context = {'notes_list': notes_list}
+    return render(request, 'notes/index.html', context)
+
+
+
+
